383e830b1f3c0a755c2622337f1552941823b161
marcospaulo46 <faxadinha@gmail.com>
2021-07-26 22:08:33 -0300

    Exercicios 26-07

---
+++ exercicio26-07/src/main/java/br/com/generation/postagem/Exercicio2607Application.java
@@ -0,0 +1,13 @@
+package br.com.generation.postagem; //asda
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercicio2607Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exercicio2607Application.class, args);
+	}
+
+}
---
+++ exercicio26-07/src/main/java/br/com/generation/postagem/controller/PostagemController.java
@@ -0,0 +1,27 @@
+package br.com.generation.postagem.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.generation.postagem.Repository.PostagemRepository;
+import br.com.generation.postagem.model.PostagemModel;
+
+@RestController
+@RequestMapping("/postagem")
+@CrossOrigin("*")
+public class PostagemController {
+	
+	@Autowired
+	private PostagemRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<PostagemModel>> GetAll(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+}
